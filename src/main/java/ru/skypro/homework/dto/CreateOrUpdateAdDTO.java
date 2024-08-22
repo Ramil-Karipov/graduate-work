b66a79e542f084ccfd71696fd592a49477a7d0aa
Ramil-Karipov <omil8@yandex.ru>
2024-08-22 12:24:15 +0500

    ver 2.4

--- src/main/java/ru/skypro/homework/dto/CreateOrUpdateAdDTO.java
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateAdDTO.java
@@ -2,10 +2,22 @@ package ru.skypro.homework.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
 @Data
 public class CreateOrUpdateAdDTO {
+    @NotBlank
+    @Size(min = 4, max = 32, message = "Title length should be between 4 and 32 symbols")
+    private String title;
 
-    String title;
-    Integer price;
-    String description;
+    @PositiveOrZero
+    @Max(value = 10_000_000, message = "Price should be between 0 and 10_000_000")
+    private int price;
+
+    @NotBlank
+    @Size(min = 8, max = 64, message = "Description length should be between 8 and 64 symbols")
+    private String description;
 }
--- src/main/java/ru/skypro/homework/dto/CreateOrUpdateCommentDTO.java
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateCommentDTO.java
@@ -2,8 +2,12 @@ package ru.skypro.homework.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class CreateOrUpdateCommentDTO {
-
+    @NotBlank
+    @Size(min = 8, max = 64, message = "Text length in comment should be between 8 and 64 symbols")
     private String text;
 }
